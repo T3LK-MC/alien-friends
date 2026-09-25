f57ddaad78a94c871da1c530a9753b43ffcf5e16
T3LK-MC <t3lk@pm.me>
2026-09-25 14:06:00 +0200

    First Commit

---
+++ src/main/java/telk/alien/frenz/AlienFriends.java
@@ -0,0 +1,18 @@
+package telk.alien.frenz;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.ResourceLocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AlienFriends implements ModInitializer {
+	public static final String MOD_ID = "alien-friends";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/telk/alien/frenz/AlienModClient.java
@@ -0,0 +1,10 @@
+package telk.alien.frenz;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlienModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
